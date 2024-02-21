336595ae3ad877da834c2aa1cc0109ef0c4a78a3
kirtias <160383931+kirtias@users.noreply.github.com>
2024-02-21 04:57:45 +0000

    datatype

---
+++ 01_basics/01_varible.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail ="kirtikushwahagamil.com"
+ var accountPassword ="237438768"
+ 
+ //accountId = 2(not allowed)
+ console.log(
+    accountId
+ );
+ accountEmail = "kiishgamil.com"
+ 
+ accountPassword ="77"
+ console.table([accountEmail,accountPassword])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,20 @@
+"use strict";// treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browers
+
+//code likhte time me space de
+
+//data type 
+//1) nuber,sring,boolean,null
+//null is standard value he
+//undifined :koe value nahi diya gaya he
+//null mean value khali he
+//2) data type(symbol)=unique ke bat karna ho to symbol ki bat karege
+//3)object
+/*let name = "kirti"
+let age = 18
+let islogin = false%*/
+//*typeof
+console.log(typeof "kirti");
+console.log(typeof null);//null is an object
+console.log(typeof undefined)//undefine is type
